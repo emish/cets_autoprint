aa79ecaa0346d3f28ebaa123733db49dd131d19d
Mishal Awadah <a.mamish@gmail.com>
2012-10-06 19:51:59 -0400

    simplified setup

--- setup.sh
+++ setup.sh
@@ -2,24 +2,6 @@
 
 cd
 
-# Make the python local library
-if ! [ -d lib ]
-then
-mkdir lib
-fi
-cd lib
-if ! [ -d python]
-then
-mkdir python
-fi
-
-cd
-
-# Setup your pythonpath correctly
-echo >> .bashrc
-echo 'PYTHONPATH=~/lib/python:$PYTHONPATH' >> .bashrc
-easy_install -d ~/lib/python pyPdf
-
 mkdir to_print
 
 git clone https://github.com/emish/cets_autoprint.git autoprint
