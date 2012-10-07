0aae8163a68d49d8f3edbfe07a28736dcad4bedf
Mishal Awadah <a.mamish@gmail.com>
2012-10-06 20:13:32 -0400

    screen problems.

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ mkdir to_print
 
 git clone https://github.com/emish/cets_autoprint.git autoprint
 
-screen -S autoprint python autoprint/autoprint.py &
+python autoprint/autoprint.py &
 
 echo "Your to_print directory is now automated. Thanks for playing."
+
+screen -d
